d2e580a365976305d3a14645f6a26a23e17c060c
Luis Cid <alexcid2002@gmail.com>
2024-06-05 19:32:15 +0100

    turtle-crossing V1.3
    Implemented scoreboard and levels to the game.
    Each time the user completes a level, meaning that he could run to the oposite location, the turtle comes back to where it spawned and the cars speed is increased

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,28 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+FONT = ("Courier", 18, "bold")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("black")
+        self.hideturtle()
+        self.score = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-280, 250)
+        self.write(f"Level: {self.score}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
